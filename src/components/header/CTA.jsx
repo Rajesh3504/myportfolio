17294ee85d9bd88587335af75492f3eb49ac06c3
RAJESH PONNAGANTI <139054007+Rajesh3504@users.noreply.github.com>
2024-08-17 12:56:42 +0530

    Update CTA.jsx

--- src/components/header/CTA.jsx
+++ src/components/header/CTA.jsx
@@ -5,7 +5,7 @@ const CTA = () => {
   return (
     <div  className='cta'>
         <a  href={CV} download className='btn'> Download CV</a>
-        <a href="#contact" className='btn btn-primary'>Get in Tounch</a>
+        <a href="#contact" className='btn btn-primary'>Get in Touch</a>
     </div>
   )
 }
