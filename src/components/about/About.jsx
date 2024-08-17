4c4ed8aec68bfb7a04282031ddb9c0f2051ccd34
RAJESH PONNAGANTI <139054007+Rajesh3504@users.noreply.github.com>
2024-08-17 12:53:58 +0530

    Update About.jsx

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -28,7 +28,7 @@ const About = () => {
               <article className='about__card'>
                 <FiUsers className='about__icon'/>
                 <h5>Certifications</h5>
-                <small>20+</small>
+                <small>8+</small>
               </article>
 
               <article className='about__card'>
