6fed18ef6d88db68fc23d7238a3845fd27c8a45e
RAJESH PONNAGANTI <139054007+Rajesh3504@users.noreply.github.com>
2024-08-17 12:55:58 +0530

    Update HeaderSocials.jsx

--- src/components/header/HeaderSocials.jsx
+++ src/components/header/HeaderSocials.jsx
@@ -5,7 +5,7 @@ import {RiInstagramFill} from 'react-icons/ri'
 const HeaderSocials = () => {
   return (
     <div className="header__socials">
-        <a href="https://www.linkedin.com/in/rajesh-p-863332235/" target='_blank'><FaLinkedin/></a>
+        <a href="https://www.linkedin.com/in/rajeshponnaganti/" target='_blank'><FaLinkedin/></a>
         <a href="https://github.com/Rajesh3504" target='_blank'><SiGithub/></a>
         <a href="https://instagram.com/zeus._.0354?igshid=NGExMmI2YTkyZg== " target='_blank'><RiInstagramFill/></a>
     </div>
